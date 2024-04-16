977168484ed26c099e3445b91a27403b651c512b
Menna Nabil <mena99nabil@gmail.com>
2024-04-16 19:24:19 +0200

    update routs

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,5 +1,6 @@
 class Routes {
   static const String onBoardingScreen = '/onBoardingScreen';
+  static const String beforeLoginScreen = '/beforeLoginScreen';
   static const String loginScreen = '/loginScreen';
   static const String signUpScreen = '/signUpScreen';
   static const String homeScreen = '/homeScreen';
