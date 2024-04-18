da37f347d3d4a83c184869a3cb8e55bf8f0efb2e
Menna Nabil <mena99nabil@gmail.com>
2024-04-18 15:40:01 +0200

    shared widget

---
+++ lib/core/shared_widgets/default_text_field.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class DefaultTextField extends StatelessWidget {
+  const DefaultTextField({super.key, required this.text, required this.labelStyle, this.suffixIcon, this.style, this.controller, this.validator, required this.isPassword});
+  final String text;
+  final TextStyle labelStyle;
+  final Widget? suffixIcon;
+  final TextStyle? style;
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
+  final bool isPassword;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: isPassword,
+      controller: controller,
+      validator: validator,
+      style: style,
+      decoration: InputDecoration(
+          labelText: text,
+          labelStyle: labelStyle,
+          suffixIcon: suffixIcon
+      ),
+    );
+  }
+}
