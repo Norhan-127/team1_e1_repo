32c2b0ad33dcf4fa31c80fd030c56a1b0a8c13b8
Norhan-127 <norhanhamza127@gmail.com>
2024-04-19 08:03:22 +0200

    add images file to core and refactor onboarding images names

---
+++ lib/core/theming/images.dart
@@ -0,0 +1,5 @@
+abstract class AppImages{
+  static const String onBoardingDragonImage='assets/images/onboarding_dragon.png';
+  static const String onBoardingCrewImage='assets/images/onboarding_crew.jpg';
+  static const String onBoardingRocketImage='assets/images/onboarding_rocket.png';
+}
--- lib/features/on_boarging/data/on_boarding_data.dart
+++ lib/features/on_boarging/data/on_boarding_data.dart
@@ -10,7 +10,7 @@ class OnBoardingData {
 }
 
 List<OnBoardingData> pages = [
-  OnBoardingData("assets/images/dragon1onboarding x1.png", AppText.dragon,AppText.sendingHumansAndCargo,true),
-  OnBoardingData("assets/images/crew-8-members.jpg", AppText.humanSpaceLight,AppText.makingLife,true),
-  OnBoardingData("assets/images/image 1.png",AppText.ready, AppText.getReady,false),
+  OnBoardingData("assets/images/onboarding_dragon.png", AppText.dragon,AppText.sendingHumansAndCargo,true),
+  OnBoardingData("assets/images/onboarding_crew.jpg", AppText.humanSpaceLight,AppText.makingLife,true),
+  OnBoardingData("assets/images/onboarding_rocket.png",AppText.ready, AppText.getReady,false),
 ];
