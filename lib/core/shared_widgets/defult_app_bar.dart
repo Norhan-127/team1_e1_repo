2ff302ff248a3abc7c7f423ed21c849d4a02fcf7
ranamohs <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-04-20 18:18:04 +0200

    make constructor after fields

--- lib/core/shared_widgets/defult_app_bar.dart
+++ lib/core/shared_widgets/defult_app_bar.dart
@@ -4,13 +4,15 @@ import 'package:team1_e1/core/theming/styles.dart';
 
 
 class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const DefaultAppBar({super.key, this.isCenter, this.icon, this.text, this.action, this.function});
+
 
   final bool ?isCenter;
   final IconData? icon;
   final String ?text;
   final List<Widget>? action;
   final void Function()? function;
+
+  const DefaultAppBar({super.key, this.isCenter, this.icon, this.text, this.action, this.function});
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
