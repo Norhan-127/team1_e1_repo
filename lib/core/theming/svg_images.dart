672f376abbae29cdf2bf1ea892fc56280749f443
Norhan-127 <norhanhamza127@gmail.com>
2024-04-22 11:57:05 +0200

    add space x logo svg to put it in the app bar

--- lib/core/theming/svg_images.dart
+++ lib/core/theming/svg_images.dart
@@ -4,4 +4,6 @@ class SvgImages{
   static const String bottomNavDragon='assets/images/bottom_nav_dragons.svg';
   static const String bottomNavCrew='assets/images/bottom_nav_crew.svg';
   static const String bottomNavLaunch='assets/images/bottom_nav_launches.svg';
+  static const String spaceXLogo='assets/images/spacex-logo.svg';
+  static const String spaceXLogoWhite='assets/images/spacex_logo_white.svg';
 }
