6e6ca26b0079382e44ea4e185fe941fce3e5af7a
Menna Nabil <mena99nabil@gmail.com>
2024-04-25 00:52:47 +0200

    add separated register form in it

--- lib/features/auth/register/ui/screens/register_screen.dart
+++ lib/features/auth/register/ui/screens/register_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../../../../core/shared_widgets/default_button.dart';
-import '../../../../../core/shared_widgets/default_text_field.dart';
-import '../../../../../core/theming/colors.dart';
-import '../../../../../core/theming/styles.dart';
+import 'package:team1_e1/features/auth/register/ui/widgets/register_form.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -19,49 +16,10 @@ class RegisterScreen extends StatelessWidget {
                 image: AssetImage("assets/images/space-galaxy-background-generative-ai 1.png"),
                 fit: BoxFit.cover),
           ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 50.0,horizontal: 40.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Let\'s',style: TextStyles.poppins40MediumWhite,),
-                  Text('Start',style: TextStyles.poppins63SemiBoldWhite,),
-                  const SizedBox(height: 150,),
-                  DefaultTextField(
-                    isPassword: false,
-                    style: TextStyles.poppins17MediumWhite,
-                    text: 'Your Name',
-                    labelStyle: TextStyles.poppins17MediumWhite,
-                    suffixIcon: const Icon(Icons.person,color: AppColors.whiteColor,),
-                  ),
-                  const SizedBox(height: 10,),
-                  DefaultTextField(
-                    isPassword: false,
-                    style: TextStyles.poppins17MediumWhite,
-                    text: 'Email Address',
-                    labelStyle: TextStyles.poppins17MediumWhite,
-                    suffixIcon: const Icon(Icons.email_outlined,color: AppColors.whiteColor,),
-                  ),
-                  const SizedBox(height: 10,),
-                  DefaultTextField(
-                    isPassword: true,
-                    style: TextStyles.poppins17MediumWhite,
-                    text: 'Password',
-                    labelStyle: TextStyles.poppins17MediumWhite,
-                    suffixIcon: const Icon(Icons.lock_outlined,color: AppColors.whiteColor,),
-                  ),
-                  const SizedBox(height: 40,),
-                  DefaultButton(
-                      radius: 20,
-                      backgroundColor: AppColors.whiteColor,
-                      text: 'Sign Up',
-                      icon: Icons.arrow_forward,
-                    style: TextStyles.poppins21MediumBlack,
-                    IconColor: Colors.black,
-                  )
-                ],
-              ),
+          child: const SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 50.0,horizontal: 40.0),
+              child: RegisterForm(),
             ),
           ),
         ),
