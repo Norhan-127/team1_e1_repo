bb1e5be28099fa33246333024111332cbf6ac722
Menna Nabil <mena99nabil@gmail.com>
2024-04-25 00:52:33 +0200

    add separated login form in it

--- lib/features/auth/login/ui/screens/login_screen.dart
+++ lib/features/auth/login/ui/screens/login_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../../../../core/shared_widgets/default_button.dart';
-import '../../../../../core/shared_widgets/default_text_field.dart';
-import '../../../../../core/theming/colors.dart';
-import '../../../../../core/theming/styles.dart';
+import 'package:team1_e1/features/auth/login/ui/widgets/login_form.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -19,41 +16,10 @@ class LoginScreen extends StatelessWidget {
                 image: AssetImage("assets/images/space-galaxy-background-generative-ai 1.png"),
                 fit: BoxFit.cover),
           ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 50.0,horizontal: 40.0),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 50.0,horizontal: 40.0),
             child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Welcome',style: TextStyles.poppins40MediumWhite,),
-                  Text('Back',style: TextStyles.poppins63SemiBoldWhite,),
-                  const SizedBox(height: 150,),
-                  DefaultTextField(
-                    isPassword: false,
-                    style: TextStyles.poppins17MediumWhite,
-                    text: 'Email Address',
-                    labelStyle: TextStyles.poppins17MediumWhite,
-                    suffixIcon: const Icon(Icons.email_outlined,color: AppColors.whiteColor,),
-                  ),
-                  const SizedBox(height: 10,),
-                  DefaultTextField(
-                    isPassword: true,
-                    style: TextStyles.poppins17MediumWhite,
-                    text: 'Password',
-                    labelStyle: TextStyles.poppins17MediumWhite,
-                    suffixIcon: const Icon(Icons.lock_outlined,color: AppColors.whiteColor,),
-                  ),
-                  const SizedBox(height: 40,),
-                  DefaultButton(
-                      radius: 20,
-                      backgroundColor: AppColors.whiteColor,
-                      text: 'Log In',
-                      icon: Icons.arrow_forward,
-                    style: TextStyles.poppins21MediumBlack,
-                    IconColor: Colors.black,
-                  )
-                ],
-              ),
+              child: LoginForm(),
             ),
           ),
         ),
