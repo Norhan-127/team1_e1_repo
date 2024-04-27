f994b5947062f9cf0f4c534ad5624e6d9307cfe3
mohamedtta <mkamed2@gmail.com>
2024-04-27 09:59:37 +0300

    provide bloc to dragon screen the app routes

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:team1_e1/core/di/dependency_injection.dart';
 import 'package:team1_e1/core/routing/routes.dart';
+import 'package:team1_e1/features/dragon/logic/dragon_cubit.dart';
 import 'package:team1_e1/features/on_boarging/ui/screens/on_baording_screen.dart';
 import 'package:team1_e1/features/rockets/ui/screens/rocket_details_screen.dart';
 import 'package:team1_e1/features/crew/ui/screen/crew_screen.dart';
@@ -8,8 +10,7 @@ import 'package:team1_e1/features/auth/login/ui/screens/login_screen.dart';
 import 'package:team1_e1/features/auth/register/ui/screens/register_screen.dart';
 import '../../features/dragon/ui/screens/dragon_details_screen.dart';
 import '../../features/dragon/ui/screens/dragon_screen.dart';
-import '../../features/on_boarging/ui/screens/on_baording_screen.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   static Route generateRoute(RouteSettings settings) {
@@ -42,7 +43,10 @@ class AppRouter {
         );
       case Routes.dragonScreen:
         return MaterialPageRoute(
-          builder: (_) => const DragonScreen(),
+            builder: (_) => BlocProvider(
+              create: (context) => getIt<DragonCubit>(),
+              child: const DragonScreen(),
+            )
 
         );
       case Routes.dragonDetailsScreen:
