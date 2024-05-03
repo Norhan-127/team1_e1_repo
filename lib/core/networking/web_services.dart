987488a3e6e43df8d15e8f0116655433f4736975
Norhan-127 <norhanhamza127@gmail.com>
2024-05-04 00:15:36 +0300

    adding launches endpoint to WebServices class

--- lib/core/networking/web_services.dart
+++ lib/core/networking/web_services.dart
@@ -3,6 +3,7 @@ import 'package:retrofit/http.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:team1_e1/core/networking/api_constants.dart';
 import 'package:team1_e1/features/dragon/data/models/dragon_response.dart';
+import 'package:team1_e1/features/launches/data/models/launches_response.dart';
 
 import '../../features/capsules/data/models/capsules_response.dart';
 part 'web_services.g.dart';
@@ -16,5 +17,8 @@ abstract class WebServices{
 
   @GET(ApiConstants.capsules)
   Future<List<Capsule>> getAllCapsules();
+  
+  @GET(ApiConstants.launches)
+  Future<List<LaunchesResponseModel>> getAllLaunches();
 
 }
