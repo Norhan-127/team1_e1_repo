cb97e4e7221fea1153b258f6995c643ee5eceb09
Norhan-127 <norhanhamza127@gmail.com>
2024-05-04 00:40:12 +0300

    adding launches cubit and repo to di

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -3,13 +3,15 @@ import 'package:get_it/get_it.dart';
 import 'package:team1_e1/core/networking/web_services.dart';
 import 'package:team1_e1/features/dragon/data/repo/dragon_repo.dart';
 import 'package:team1_e1/features/dragon/logic/dragon_cubit.dart';
+import 'package:team1_e1/features/launches/logic/launches_cubit.dart';
 import '../../features/capsules/data/repo/capsules_repo.dart';
 import '../../features/capsules/logic/capsule_cubit.dart';
+import '../../features/launches/data/repo/launches_repo.dart';
 import '../networking/dio_factory.dart';
 
-
 final getIt = GetIt.instance;
-Future<void> setUpGetIt()async{
+
+Future<void> setUpGetIt() async {
   Dio dio = await DioFactory.getDio();
   getIt.registerLazySingleton<WebServices>(() => WebServices(dio));
 
@@ -17,10 +19,11 @@ Future<void> setUpGetIt()async{
   getIt.registerLazySingleton<DragonRepo>(() => DragonRepo(getIt()));
   getIt.registerLazySingleton<DragonCubit>(() => DragonCubit(getIt()));
 
-
   //---------------Capsules----------------------
   getIt.registerLazySingleton<CapsuleRepo>(() => CapsuleRepo(getIt()));
   getIt.registerLazySingleton<CapsuleCubit>(() => CapsuleCubit(getIt()));
 
-
+  //---------------Launches----------------------
+  getIt.registerLazySingleton<LaunchesRepo>(() => LaunchesRepo(getIt()));
+  getIt.registerLazySingleton<LaunchesCubit>(() => LaunchesCubit(getIt()));
 }
