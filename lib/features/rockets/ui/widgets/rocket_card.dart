af73fb6ae8f88cb5aec589f3a7eb07bef07dd1af
ranamohs <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-05-04 20:05:09 +0300

    replace static data in one card with api data

--- lib/features/rockets/ui/widgets/rocket_card.dart
+++ lib/features/rockets/ui/widgets/rocket_card.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:team1_e1/core/theming/colors.dart';
 import 'package:team1_e1/core/theming/styles.dart';
-import 'package:team1_e1/features/rockets/ui/screens/rocket_details_screen.dart';
 
 
 class RocketCard extends StatelessWidget {
-  const RocketCard({super.key});
+  List<String>? image;
+  String? title;
+  String? description;
+
+  RocketCard(
+      {super.key,
+      required this.image,
+      required this.title,
+      required this.description});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 35.0,vertical: 18.0),
+      padding: const EdgeInsets.symmetric(horizontal: 35.0, vertical: 18.0),
       child: Container(
-        height: 265,
+        height: 300,
         decoration: BoxDecoration(
           border: Border.all(color: AppColors.lightGrey),
           borderRadius: const BorderRadius.all(Radius.circular(20)),
@@ -20,26 +28,29 @@ class RocketCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Image(
-              image: AssetImage('assets/images/rocket_details.png'),
+            ClipRRect(
+              borderRadius: const BorderRadius.only(topRight: Radius.circular(12.0) , topLeft: Radius.circular(12.0)),
+              child: Image.network(
+                            image![0],
+                            height: 120.h,
+                            fit: BoxFit.cover,
+                            width: 300.w,
+                          ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => const RocketDetailsScreen(),),);
-                },
-                child:Column(
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Falcon Heavy',style: TextStyles.font22RegularWhite,),
                     Text(
-                      'The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth',
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyles.SmallWhite),
+                      title!,
+                      style: TextStyles.font22RegularWhite,
+                    ),
+                    Text(description!,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyles.SmallWhite),
                   ],
                 ),
               ),
