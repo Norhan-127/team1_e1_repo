1ee8e5d20ce9b8fd00245e16a44dd5d78b38470c
ranamohs <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-05-04 19:55:17 +0300

    added BlocProvider on Rocket screen

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -5,20 +5,19 @@ import 'package:team1_e1/features/capsules/logic/capsule_cubit.dart';
 import 'package:team1_e1/features/capsules/ui/screens/capsules_screen.dart';
 import 'package:team1_e1/features/dragon/logic/dragon_cubit.dart';
 import 'package:team1_e1/features/on_boarging/ui/screens/on_baording_screen.dart';
+import 'package:team1_e1/features/rockets/data/models/rockets_response.dart';
+import 'package:team1_e1/features/rockets/logic/rocket_cubit.dart';
 import 'package:team1_e1/features/rockets/ui/screens/rocket_details_screen.dart';
 import 'package:team1_e1/features/crew/ui/screen/crew_screen.dart';
 import 'package:team1_e1/features/auth/login/ui/screens/before_login.dart';
 import 'package:team1_e1/features/auth/login/ui/screens/login_screen.dart';
 import 'package:team1_e1/features/auth/register/ui/screens/register_screen.dart';
+import 'package:team1_e1/features/rockets/ui/screens/rockets_screen.dart';
 import '../../features/capsules/data/models/capsules_response.dart';
 import '../../features/capsules/ui/screens/one_capsule_details_screen.dart';
 import '../../features/dragon/ui/screens/dragon_details_screen.dart';
 import '../../features/dragon/ui/screens/dragon_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-
-
-
 
 class AppRouter {
   static Route generateRoute(RouteSettings settings) {
@@ -28,11 +27,22 @@ class AppRouter {
           builder: (_) => const OnBoardingScreen(),
         );
 
-      case Routes.rocketDetailsScreen:
+      case Routes.rocketScreen:
         return MaterialPageRoute(
-          builder: (_) => const RocketDetailsScreen(),
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<RocketCubit>(),
+            child: const RocketsScreen(),
+          ) ,
         );
-        case Routes.crewScreen:
+
+      case Routes.rocketDetailsScreen:
+        final rocket = settings.arguments as Rocket;
+        return MaterialPageRoute(
+          builder: (_) =>  RocketDetailsScreen(
+            rocket: rocket,
+          ),
+        );
+      case Routes.crewScreen:
         return MaterialPageRoute(
           builder: (_) => const CrewScreen(),
         );
@@ -46,36 +56,31 @@ class AppRouter {
         );
       case Routes.signUpScreen:
         return MaterialPageRoute(
-            builder: (_) => const RegisterScreen(),
-
+          builder: (_) => const RegisterScreen(),
         );
       case Routes.dragonScreen:
         return MaterialPageRoute(
             builder: (_) => BlocProvider(
-              create: (context) => getIt<DragonCubit>(),
-              child: const DragonScreen(),
-            )
-
-        );
+                  create: (context) => getIt<DragonCubit>(),
+                  child: const DragonScreen(),
+                ));
       case Routes.dragonDetailsScreen:
         return MaterialPageRoute(
           builder: (_) => const DragonDetailsScreen(),
-
         );
       case Routes.capsuleScreen:
         return MaterialPageRoute(
-          builder: (_) => BlocProvider(
-              create: (context) => getIt<CapsuleCubit>(),
-            child: const CapsulesScreen(),
-          )
-
-        );
+            builder: (_) => BlocProvider(
+                  create: (context) => getIt<CapsuleCubit>(),
+                  child: const CapsulesScreen(),
+                ));
 
       case Routes.capsuleDetailsScreen:
         final capsule = settings.arguments as Capsule;
         return MaterialPageRoute(
-            builder: (_) =>  OneCapsuleDetailsScreen(capsule: capsule,)
-        );
+            builder: (_) => OneCapsuleDetailsScreen(
+                  capsule: capsule,
+                ));
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
