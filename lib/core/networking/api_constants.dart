f11840dbd9fcc4dba5262bc20a64ca8e55c69532
Menna Nabil <mena99nabil@gmail.com>
2024-05-04 23:20:48 +0300

    add crew end point

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -2,4 +2,5 @@ class ApiConstants{
   static const String baseUrl = 'https://api.spacexdata.com/v4/';
   static const String dragons = 'dragons';
   static const String capsules = 'capsules';
+  static const String crew = 'crew';
 }
