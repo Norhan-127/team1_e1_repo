802d9df018ac1b6276aab930d8b166d1db4f6b08
ranamohs <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-05-05 17:30:09 +0300

    create company screen file only

--- lib/features/about_company/ui/screens/about_company_screen.dart
+++ lib/features/about_company/ui/screens/about_company_screen.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:team1_e1/core/shared_widgets/background_container.dart';
-import 'package:team1_e1/core/theming/colors.dart';
+
 import 'package:team1_e1/core/theming/styles.dart';
 
 class CompanyScreen extends StatelessWidget {
