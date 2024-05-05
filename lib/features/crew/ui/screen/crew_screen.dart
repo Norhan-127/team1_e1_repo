9ffe39ac9d3a7ef00d7adfbc86f9a5b1225d9caa
ranamohs <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-05-05 17:32:30 +0300

    edite appbar in crew screen

--- lib/features/crew/ui/screen/crew_screen.dart
+++ lib/features/crew/ui/screen/crew_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:team1_e1/core/theming/colors.dart';
 import '../../../../core/theming/styles.dart';
 import '../widgets/crew_list.dart';
 
@@ -9,26 +10,35 @@ class CrewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/app_background.png"),
-                fit: BoxFit.cover),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 50.0),
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/app_background.png"),
+                  fit: BoxFit.cover),
+            ),
+             child: Column(
+               children: [
+                 AppBar(
+                   leading: IconButton(
+                     onPressed: (){
+                       Navigator.pop(context);
+                     },
+                     icon: Icon(Icons.arrow_back,color: AppColors.whiteColor,size: 25,),
+                   ),
+                   backgroundColor: Colors.transparent,
+                   title: Text('SpaceX Crew',style: TextStyles.poppins28BoldWhite,),
+                 ),
+                 SizedBox(height: 20,),
+                 CrewList(),
+               ],
+             ),
           ),
-           child: Column(
-             children: [
-               AppBar(
-                 backgroundColor: Colors.transparent,
-                 title: Text('SpaceX Crew',style: TextStyles.poppins28BoldWhite,),
-               ),
-               SizedBox(height: 20,),
-               CrewList(),
-             ],
-           ),
         ),
       ),
     );
