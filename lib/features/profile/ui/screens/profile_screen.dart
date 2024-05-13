a5d1764ac4eb5be23634cd98420067cb2de98b21
Menna Nabil <mena99nabil@gmail.com>
2024-05-13 13:37:00 +0300

    add dynamic data in profile screen

--- lib/features/profile/ui/screens/profile_screen.dart
+++ lib/features/profile/ui/screens/profile_screen.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:team1_e1/core/theming/colors.dart';
-
+import 'package:team1_e1/features/auth/login/ui/screens/before_login.dart';
+import 'package:team1_e1/features/profile/logic/profile_cubit.dart';
+import 'package:team1_e1/features/profile/logic/profile_state.dart';
 import '../../../../core/theming/styles.dart';
 import '../widgets/image_circle.dart';
 import '../widgets/main_row.dart';
@@ -12,75 +16,123 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Text('Profile',style: TextStyles.poppins19MediumWhite,),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new_outlined,color: AppColors.whiteColor,),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 25.0,horizontal: 20),
-        child: Column(
-          children: [
-            Container(
-              width: 300.w,
-              height: 260.h,
-              decoration: BoxDecoration(
-                  color: AppColors.lightGrey.withOpacity(.18),
-                borderRadius: const BorderRadius.all(Radius.circular(20))
+    return BlocProvider(
+      create: (context) => ProfileCubit()..getUserData(),
+      child: BlocBuilder<ProfileCubit, ProfileState>(
+        builder: (context, state) {
+          return Scaffold(
+            backgroundColor: Colors.black,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              title: Text(
+                'Profile',
+                style: TextStyles.poppins19MediumWhite,
               ),
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.arrow_back_ios_new_outlined,
+                  color: AppColors.whiteColor,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            body: ProfileCubit.get(context).userModel != null ?
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 25.0, horizontal: 20),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const ImageCircle(),
-                  const SizedBox(height: 15,),
-                  Text('Bryan Wolf',style: TextStyles.poppins19MediumWhite,),
-                  const SizedBox(height: 7,),
-                  Text('bryanwolf@gmail.com',style: TextStyles.poppins16WhiteFontWeight300,),
+                  Container(
+                    width: 300.w,
+                    height: 260.h,
+                    decoration: BoxDecoration(
+                        color: AppColors.lightGrey.withOpacity(.18),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20))),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const ImageCircle(),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          ProfileCubit.get(context).userModel!.name!,
+                          style: TextStyles.poppins19MediumWhite,
+                        ),
+                        const SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          ProfileCubit.get(context).userModel!.email!,
+                          style: TextStyles.poppins16WhiteFontWeight300,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: 300.w,
+                    height: 60.h,
+                    decoration: BoxDecoration(
+                        color: AppColors.lightGrey.withOpacity(.18),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(15))),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: MainRow(
+                        text: 'Edit Profile',
+                        icon: Icons.person_pin_circle_outlined,
+                        function: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const EditProfileScreen(),
+                            )),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                      width: 300.w,
+                      height: 60.h,
+                      decoration: BoxDecoration(
+                          color: AppColors.lightGrey.withOpacity(.18),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(15))),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: MainRow(
+                          text: 'Logout',
+                          icon: Icons.logout,
+                          function: () async {
+                            await FirebaseAuth.instance.signOut();
+                            Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const BeforeLoginScreen(),
+                                ),
+                                (route) => false);
+                          },
+                        ),
+                      )),
                 ],
               ),
-            ),
-            const SizedBox(height: 10,),
-            Container(
-              width: 300.w,
-              height: 60.h,
-              decoration: BoxDecoration(
-                  color: AppColors.lightGrey.withOpacity(.18),
-                  borderRadius: const BorderRadius.all(Radius.circular(15))
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: MainRow(text: 'Edit Profile',icon: Icons.person_pin_circle_outlined,
-                  function: ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => const EditProfileScreen(),)),
-                ),
+            ) :
+            const Center(
+              child: CircularProgressIndicator(
+                color: AppColors.whiteColor,
               ),
             ),
-            const SizedBox(height: 10,),
-            Container(
-                width: 300.w,
-                height: 60.h,
-                decoration: BoxDecoration(
-                    color: AppColors.lightGrey.withOpacity(.18),
-                    borderRadius: const BorderRadius.all(Radius.circular(15))
-                ),
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: MainRow(
-                    text: 'Logout',icon: Icons.logout,
-                  ),
-                )
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
 }
-
-
