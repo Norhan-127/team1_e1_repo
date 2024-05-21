3e09b053c0e4ffd288f00ceb718b98de92e21369
Menna Nabil <mena99nabil@gmail.com>
2024-05-21 23:08:12 +0300

    UI refactor

--- lib/features/profile/ui/screens/edit_profile_screen.dart
+++ lib/features/profile/ui/screens/edit_profile_screen.dart
@@ -21,9 +21,11 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
 
   late TextEditingController nameController;
   late TextEditingController emailController;
+  late GlobalKey<FormState> formKey;
 
   @override
   void initState() {
+    formKey = GlobalKey<FormState>();
     nameController = TextEditingController();
     emailController = TextEditingController();
     super.initState();
@@ -58,62 +60,73 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    if(state is UpdateUserDataLoading)
-                      LinearProgressIndicator(),
-                      SizedBox(height: 10,),
-                    const ImageCircle(),
-                    const SizedBox(height: 30,),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Email', style: TextStyles.fontSpace18RegularWhite,),
-                        const SizedBox(height: 8,),
-                        DefaultTextField(
-                          controller: emailController,
-                          readOnly: true,
-                          filled: true,
-                          fillColor: Colors.transparent,
-                          radius: 20,
-                          style: TextStyles.fontSpace16RegularWhite,
-                          isPassword: false,
-                        ),
-                        const SizedBox(height: 15,),
-                        Text(
-                          'User Name', style: TextStyles.fontSpace18RegularWhite,),
-                        const SizedBox(height: 8,),
-                        DefaultTextField(
-                          controller: nameController,
-                          readOnly: false,
-                          filled: true,
-                          fillColor: Colors.transparent,
-                          radius: 20,
-                          borderSide: BorderSide.none,
-                          style: TextStyles.fontSpace16RegularWhite,
-                          isPassword: false,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 30,),
-                    DefaultButton(
-                        function: (){
-                          ProfileCubit.get(context).updateUser(name: nameController.text);
-                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProfileScreen(),));
-                        },
-                        radius: 20,
-                        text: 'Edit',
-                        style: TextStyles.fontSpace22RegularWhite,
-                        gradient: RadialGradient(
-                        center: Alignment.bottomRight,
-                        radius: 4.4,
-                        colors: [
-                          AppColors.lightBlueColor,
-                          AppColors.deepPurpleColor
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      if(state is UpdateUserDataLoading)
+                        LinearProgressIndicator(),
+                        SizedBox(height: 10,),
+                      const ImageCircle(),
+                      const SizedBox(height: 30,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Email', style: TextStyles.fontSpace18RegularWhite,),
+                          const SizedBox(height: 8,),
+                          DefaultTextField(
+                            controller: emailController,
+                            readOnly: true,
+                            filled: true,
+                            fillColor: Colors.transparent,
+                            radius: 20,
+                            style: TextStyles.fontSpace16RegularWhite,
+                            isPassword: false,
+                          ),
+                          const SizedBox(height: 15,),
+                          Text(
+                            'User Name', style: TextStyles.fontSpace18RegularWhite,),
+                          const SizedBox(height: 8,),
+                          DefaultTextField(
+                            validator: (val) {
+                              if (val == '') {
+                                return "name should not empty";
+                              }
+                              return null;
+                            },
+                            controller: nameController,
+                            readOnly: false,
+                            filled: true,
+                            fillColor: Colors.transparent,
+                            radius: 20,
+                            borderSide: BorderSide.none,
+                            style: TextStyles.fontSpace16RegularWhite,
+                            isPassword: false,
+                          ),
                         ],
                       ),
-                    )
-                  ],
+                      const SizedBox(height: 30,),
+                      DefaultButton(
+                          function: (){
+                            if (formKey.currentState!.validate()) {
+                              ProfileCubit.get(context).updateUser(name: nameController.text);
+                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProfileScreen(),));
+                            }
+                          },
+                          radius: 20,
+                          text: 'Edit',
+                          style: TextStyles.fontSpace22RegularWhite,
+                          gradient: RadialGradient(
+                          center: Alignment.bottomRight,
+                          radius: 4.4,
+                          colors: [
+                            AppColors.lightBlueColor,
+                            AppColors.deepPurpleColor
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ) :
